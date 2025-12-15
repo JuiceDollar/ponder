81b8b7485e34f81dd73f120f19acc3f36bb66ce2
Danswar <48102227+Danswar@users.noreply.github.com>
2025-12-15 11:35:38 -0300

    Contracts v1.0.6 (#6)

--- ponder.config.ts
+++ ponder.config.ts
@@ -23,8 +23,8 @@ export const ADDR = ADDRESS[Id]!;
 export const CONFIG = {
 	[Id]: {
 		rpc: process.env.RPC_URL_MAINNET ?? chain.rpcUrls.default.http[0],
-		startStablecoin: 18981881,
-		startMintingHubV2: 18981881,
+		startStablecoin: 19497714,
+		startMintingHubV2: 19497714,
 		blockrange: 1000,
 		maxRequestsPerSecond: 50,
 		pollingInterval: 5_000,
